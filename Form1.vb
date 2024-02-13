8ce26ef5763f984d1f12333272601bac06c5fee2
alwifahru <fahrualwi22@gmail.com>
2024-02-13 16:50:41 +0700

    first init github

---
+++ Form1.vb
@@ -0,0 +1,17 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim writefile As StreamWriter
+        writefile = File.CreateText(TextBox1.Text)
+        Dim writedata As String = TextBox2.Text
+        writefile.WriteLine(writedata)
+        writefile.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim readfile As String
+        readfile = My.Computer.FileSystem.ReadAllText(TextBox1.Text)
+        TextBox2.Text = readfile
+    End Sub
+End Class
